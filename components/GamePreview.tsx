04f52c94e2a88bcf42e1615ad7108182fac32d9e
Awointa <akosawointa@gmail.com>
2025-07-01 15:59:12 +0100

    refac :  replaced card with HuntCards component  instance

--- components/GamePreview.tsx
+++ components/GamePreview.tsx
@@ -1,10 +1,9 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Eye, ArrowRight } from "lucide-react"
-import Image from "next/image"
+import { Eye} from "lucide-react"
 import PlayCircle from "@/components/icons/PlayCircle"
+import { HuntCards } from "./HuntCards"
 
 interface Hunt {
   id: number
@@ -33,34 +32,7 @@ export function GamePreview({ hunts }: GamePreviewProps) {
         </div>
       </div>
 
-      <div className="">
-        {hunts.slice(0, 2).map((hunt, index) => (
-          <div
-            key={hunt.id}
-            className={`rounded-tl-2xl rounded-tr-2xl p-4 text-white bg-gradient-to-b from-[#3737A4] to-[#0C0C4F]`}
-          >
-            <div className="text-right text-[#B3B3E5] text-sm mb-2">{index + 1}/10</div>
-            <h3 className="text-lg font-bold mb-2">
-              {hunt.title || (index === 0 ? "Title of the hunt" : "What is the fastets bird?")}
-            </h3>
-            <p className="text-sm opacity-90 mb-4">
-              {hunt.description ||
-                (index === 0
-                  && "Description")}
-            </p>
-            {index === 0 ? (
-             ""
-            ) : (
-             <Image src={hunt.link} alt="Logo" width={96} height={96} />
-            )}
-          </div>
-        ))}
-
-        <div className="bg-white flex gap-2 p-6 rounded-bl-2xl rounded-br-2xl">
-          <Input placeholder="Enter code to unlock" className="flex-1 px-4 py-2 rounded-full" />
-          <Button className="bg-gradient-to-b from-[#3737A4] to-[#0C0C4F] hover:bg-purple-700 text-white px-6 py-2 rounded-xl"><ArrowRight className="w-4 h-4" /></Button>
-        </div>
-      </div>
+      <HuntCards hunts={hunts} />
     </div>
   )
 }
